e03a4b64afe020a50a262a2512805eb2f536f42e
Themko30 <alfonsodalbenzio5@gmail.com>
2024-10-16 16:26:13 +0200

    Da Sai

---
+++ poetry-club-door-policy/door-policy.js
@@ -0,0 +1,72 @@
+// @ts-check
+//
+// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion on the web
+// and supported IDEs when implementing this exercise. You don't need to
+// understand types, JSDoc, or TypeScript in order to complete this JavaScript
+// exercise, and can completely ignore this comment block and directive.
+//
+// 👋🏽 Hi again!
+//
+// A quick reminder about exercise stubs:
+//
+// 💡 You're allowed to completely clear any stub before you get started. Often
+// we recommend using the stub, because they are already set-up correctly to
+// work with the tests, which you can find in ./door-policy.spec.js.
+//
+// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
+// in idiomatic JavaScript, but some companies and style-guides do enforce them.
+//
+// Good luck with that door policy!
+
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the front door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export function frontDoorResponse(line) {
+  let conCat = "";
+  return (conCat = line[0] + conCat);
+}
+
+/**
+ * Format the password for the front-door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the front door password
+ */
+export function frontDoorPassword(word) {
+  let firstLetter = "" + word[0].toUpperCase();
+  let otherLetters = "";
+  for (let i = 1; i < word.length; i++) {
+    otherLetters = otherLetters + word[i].toLowerCase();
+  }
+  return firstLetter + otherLetters;
+}
+
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the back door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export function backDoorResponse(line) {
+  let trimmed = line.trim();
+  let conCat = "";
+  return (conCat = trimmed[trimmed.length - 1] + conCat);
+}
+
+/**
+ * Format the password for the back door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the back door password
+ */
+export function backDoorPassword(word) {
+  return frontDoorPassword(word) + ", please";
+}
